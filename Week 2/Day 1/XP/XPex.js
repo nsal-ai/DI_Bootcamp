e5067120e756ec52faa1c7a238dacb335466f278
nsal-ai <nigel.salem@rocketmail.com>
2021-04-11 13:06:55 +0100

    XP Exercise JS

---
+++ Week 2/Day 1/XP/XPex.js
@@ -0,0 +1,25 @@
+let a = 5;
+let b = "34";
+let c = a + b;
+
+console.log(c); //logs 534
+
+let value = 5
+let string = "4"
+
+let outcome = value - string;
+console.log(outcome); //logs 1
+
+console.log(10%5);
+console.log(5%10); //logs 5 remainder
+console.log("Java"+"Script");
+console.log(" " + " ");
+console.log(" " + 0);
+console.log(true + true);
+console.log(true + false);
+console.log(false + false);
+console.log(false + true);
+console.log(3-4);
+console.log("Bob"- "Bill"); //returns NaN
+
+("Bob" - "Bill").isNaN(); //??
