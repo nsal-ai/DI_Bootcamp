7f6b05bea852c8d8616392129992410e7fff4635
nsal-ai <nigel.salem@rocketmail.com>
2021-04-11 23:47:34 +0100

    XP Gold week 2 day 1

---
+++ Week 2/Day 1/XPgold/XPgold.js
@@ -0,0 +1,64 @@
+//Exercise 1
+
+// let me = ["my", "favorite", "color", "blue"]
+// let meString = me.toString();
+
+// let meJoin = me.join('');
+// console.log(meJoin); //joins items in array without commas or space
+
+// Exercise 2
+
+// let str1 = "Salt"
+// let str2 = "Pepper"
+
+// let newSTR1 = str2.substring(0, 2) + str1.substring(2, 4);
+// let newSTR2 = str1.substring(0, 2) + str2.substring(2, 6);
+// console.log(newSTR1);
+// console.log(newSTR2);
+
+
+
+// let str3 = newSTR1 + " " + newSTR2;
+
+// console.log(str3);
+
+
+
+//Exercise 2
+
+
+//prompt("first input figure"); will return Nan
+//prompt("second input figure"); 
+//var num1 = parseFloat(prompt("first input figure")); start from this line
+//var num2 = parseFloat(prompt("second input figure"));
+
+//var sum = num1 + num2;
+//alert(sum);
+
+
+//Bonus
+//import operator
+//ops = {"+":operator.add, "-":operator.sub, "*":operator.multiply, "/":operator.divide}
+//console.log ops["/"](10,2); start again from line 45
+
+
+//var num1 = parseFloat(prompt("first input")); 3 inputs from this point to make computation
+//var operator = prompt(["+", "-", "*", "/"]);
+//var num2 = parseFloat(prompt("second input"));
+
+//alert(eval(num1 + operator + num2));
+
+
+
+
+
+
+
+
+   
+
+
+    
+    
+    
+
