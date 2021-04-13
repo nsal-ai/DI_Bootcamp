b72561c9d5b0e37cc2acb80fb44605cc20bd39c0
nsal-ai <nigel.salem@rocketmail.com>
2021-04-13 20:20:37 +0100

    Daily_Chal_Stars

---
+++ Week 2/Day 3/DailyChall/Stars1.js
@@ -0,0 +1,22 @@
+/*
+stars = ["*", "**", "***", "****", "*****", "******"];
+
+var i = stars.length;
+
+for (i = 0; i < 6; i++){
+    console.log(stars[i]);
+   
+}
+*/
+
+
+/*
+var txt = "*"
+for (i = 0; i < 1; i++){
+    console.log(txt);
+
+ for (counter = 0; counter < 5; counter++){
+     console.log(txt+="*");
+ }
+}
+*/
