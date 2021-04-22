cc633b20c9d9fc8b06f9f48f768f94518f087dfd
nsal-ai <nigel.salem@rocketmail.com>
2021-04-22 08:07:58 +0100

    W3 D3 Challenges

---
+++ Week 3/Day 3/XP/moveBox.js
@@ -0,0 +1,35 @@
+// var x = target.clientX;     // Get the horizontal coordinate
+// var y = target.clientY;     // Get the vertical coordinate
+// var coor = "X coords: " + x + ", Y coords: " + y;
+
+
+// square1 listeners
+
+let square1 = document.getElementById('square1');
+
+//square1.addEventListener('dragstart', dragStart);
+//square1.addEventListener('dragdrop', dragDrop);
+
+//Drag functions
+
+
+let draggable = document.querySelector('draggable');
+
+// draggable.forEach(draggable => {
+//     draggable.addEventListener('dragstart', () => {
+//         console.log('drag start');
+//     })
+// })
+
+// function dragstart(event) {
+//     let dragStart = document.getElementsByClassName('dragStart');
+//     document.getElementById('square1');
+//     event.dataTransfer.setData('text/plain', event.target.id);
+//     this.classname += ' hold';
+//     this.classname = 'invisible';
+// }
+
+
+function dragDrop() {
+
+}
